b9fe1c30035f76c8e6b798e8e22ec8b074ea4c57
Ken Alex Fassone <nexusventuri@gmail.com>
2019-07-13 20:03:03 +0100

    Remove print out and debugging
    I'd like to get a command/sub-command namespace for bash script. So
    users of the script can:
    
    - Get a list of available commands `playground help`
    - Invoke a command. e.g. `playground command`
    - Get a list of available commands `playground help command`
    - Invoke a sub command. e.g. `playground go left`
    - Invoke a sub command. e.g. `playground help go left`
    
    Have a nice way to organize the code.

--- other/function_invokation_by_main_source.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-require "lib/a_lib_file"
-require_relative "a_relative_file_to_be_sourced"
--- test.sh
+++ test.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-lib="lib/helpers.sh"
-source "$lib"
+source "lib/helpers.sh"
 
-script="other/function_invokation_by_main_source.sh"
-echo ""
-echo ""
-echo "using source"
-echo ""
-source "$script"
-echo ""
-echo ""
-echo ""
-echo ""
-
-echo ""
-echo ""
-echo "using actual invocation"
-echo ""
 $script
-echo ""
-echo ""
-echo ""
-echo ""
-
-echo ""
-echo ""
-echo "using $ ( )"
-echo ""
-echo $($script)
-echo ""
-echo ""
-echo ""
-echo ""
